15cefcdc736bae6b2cc96e27de8d5291be91941b
tkurc <tkurc>
2019-02-03 15:43:14 -0500

    Integrating ADIOS2 to conversion program. Check in for backup.

---
+++ tools/adios2pio-a2/adios2pio-nm.cxx
@@ -0,0 +1,45 @@
+#include <mpi.h>
+#ifdef TIMING
+#include <gptl.h>
+#endif
+#include "adios2pio-nm-lib.h"
+
+int main (int argc, char *argv[])
+{
+    int ret = 0;
+
+    MPI_Init(&argc, &argv);
+
+	int mpirank;
+	MPI_Comm comm_in = MPI_COMM_WORLD;
+    MPI_Comm_rank(comm_in, &mpirank);
+
+    if (argc < 5) {
+		if (!mpirank) usage_nm(argv[0]);
+        return 1;
+    }
+
+    string infilepath  = argv[1];
+    string outfilename = argv[2];
+	string piotype     = argv[3];
+	int    mem_opt     = atoi(argv[4]);
+	
+#ifdef TIMING
+    /* Initialize the GPTL timing library. */
+    if ((ret = GPTLinitialize ()))
+        return ret;
+#endif
+
+	SetDebugOutput(1);
+	ret = ConvertBPToNC(infilepath,outfilename,piotype,mem_opt,comm_in);
+
+#ifdef TIMING
+    /* Finalize the GPTL timing library. */
+    if ((ret = GPTLfinalize ()))
+        return ret;
+#endif
+
+    MPI_Finalize();
+    return ret;
+}
+
